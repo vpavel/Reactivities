445075c728199cf234ade63dc490ed8aaff72cde
Victor Pavel <victor.pavel25@gmail.com>
2023-07-23 12:19:58 +0300

    add mobx store

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -2,9 +2,14 @@ import ReactDOM from 'react-dom/client';
 import 'semantic-ui-css/semantic.min.css';
 import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
+import { StoreContext, store } from './app/stores/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(<App />);
+root.render(
+	<StoreContext.Provider value={store}>
+		<App />
+	</StoreContext.Provider>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
