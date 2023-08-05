5b350d6219b4afc60be8e81474b47acc871f750c
Victor Pavel <victor.pavel25@gmail.com>
2023-08-05 17:04:18 +0300

    add validation error handling

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -9,21 +9,21 @@ namespace API.Controllers
   {
 
     [HttpGet] //api/activities
-    public async Task<ActionResult<List<Activity>>> GetActivities()
+    public async Task<IActionResult> GetActivities()
     {
-      return await Mediator.Send(new List.Query());
+      return HandleResult(await Mediator.Send(new List.Query()));
     }
 
     [HttpGet("{id}")] //api/activities/{id}
-    public async Task<ActionResult<Activity>> GetActivity(Guid id)
+    public async Task<IActionResult> GetActivity(Guid id)
     {
-      return await Mediator.Send(new Details.Query { Id = id });
+      return HandleResult(await Mediator.Send(new Details.Query { Id = id }));
     }
 
     [HttpPost]
     public async Task<IActionResult> CreateActivity(Activity activity)
     {
-      return Ok(await Mediator.Send(new Create.Command { Activity = activity }));
+      return HandleResult(await Mediator.Send(new Create.Command { Activity = activity }));
     }
 
     [HttpPut("{id}")]
@@ -36,7 +36,7 @@ namespace API.Controllers
     [HttpDelete("{id}")]
     public async Task<IActionResult> DeleteActivity(Guid id)
     {
-      return Ok(await Mediator.Send(new Delete.Command { Id = id }));
+      return HandleResult(await Mediator.Send(new Delete.Command { Id = id }));
     }
   }
 
--- client-app/src/app/layout/Navbar.tsx
+++ client-app/src/app/layout/Navbar.tsx
@@ -10,6 +10,7 @@ export default function Navbar() {
 					Reactivities
 				</Menu.Item>
 				<Menu.Item as={NavLink} to="/activities" name="Activities" />
+				<Menu.Item as={NavLink} to="/errors" name="Errors" />
 				<Menu.Item>
 					<Button as={NavLink} to="/createActivity" positive content="Create activity" />
 				</Menu.Item>
