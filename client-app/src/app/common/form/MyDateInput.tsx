8566299d7745a5ac46dd0bce03f3db974d3f9ec7
Victor Pavel <victor.pavel25@gmail.com>
2023-10-01 00:48:40 +0300

    add formik and form validation

---
+++ client-app/src/app/common/form/MyDateInput.tsx
@@ -0,0 +1,23 @@
+import { useField } from "formik";
+import ReactDatePicker, { ReactDatePickerProps } from "react-datepicker";
+import { Form, Label } from "semantic-ui-react";
+
+export default function MyDateInput(props: Partial<ReactDatePickerProps>) {
+  const [field, meta, helpers] = useField(props.name!);
+
+  return (
+    <Form.Field error={meta.touched && !!meta.error}>
+      <ReactDatePicker
+        {...field}
+        {...props}
+        selected={(field.value && new Date(field.value)) || null}
+        onChange={(value) => helpers.setValue(value)}
+      />
+      {meta.touched && meta.error ? (
+        <Label basic color="red">
+          {meta.error}
+        </Label>
+      ) : null}
+    </Form.Field>
+  );
+}
