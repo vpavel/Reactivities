32a2efc70ff7017ea8c494c0bf9d11c56f255332
vpavel <victor.pavel25@gmail.com>
2024-03-19 12:48:22 +0200

    add photo backend

--- Application/Core/MappingProfiles.cs
+++ Application/Core/MappingProfiles.cs
@@ -15,7 +15,10 @@ namespace Application.Core
       CreateMap<ActivityAttendee, Profiles.Profile>()
         .ForMember(d => d.DisplayName, o => o.MapFrom(s => s.AppUser.DisplayName))
         .ForMember(d => d.Username, o => o.MapFrom(s => s.AppUser.UserName))
-        .ForMember(d => d.Bio, o => o.MapFrom(s => s.AppUser.Bio));
+        .ForMember(d => d.Bio, o => o.MapFrom(s => s.AppUser.Bio))
+        .ForMember(d => d.Image, o => o.MapFrom(s => s.AppUser.Photos.FirstOrDefault(x => x.IsMain).Url));
+      CreateMap<AppUser, Profiles.Profile>()
+        .ForMember(d => d.Image, o => o.MapFrom(s => s.Photos.FirstOrDefault(x => x.IsMain).Url));
     }
 
   }
