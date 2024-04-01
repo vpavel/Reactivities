7fa179dc4a861a194997d8c8eb217157e8e9d2b4
vpavel <victor.pavel25@gmail.com>
2024-04-01 20:58:39 +0300

    change to vite, add route auth

--- API/Program.cs
+++ API/Program.cs
@@ -35,6 +35,7 @@ app.UseCors("CorsPolicy");
 
 app.UseAuthentication();
 app.UseAuthorization();
+app.UseStaticFiles();
 
 app.MapControllers();
 app.MapHub<ChatHub>("/chat");
--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -1,9 +1,8 @@
 import ReactDOM from "react-dom/client";
 import "semantic-ui-css/semantic.min.css";
 import "react-calendar/dist/Calendar.css";
-import "react-datepicker/dist/react-datepicker.module.css";
+import "react-datepicker/dist/react-datepicker.css";
 import "react-toastify/dist/ReactToastify.min.css";
-import reportWebVitals from "./reportWebVitals";
 import { StoreContext, store } from "./app/stores/store";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./app/router/Routes";
@@ -14,8 +13,3 @@ root.render(
     <RouterProvider router={router} />
   </StoreContext.Provider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
